79ab083bee75e88da21b45360abf3930bb1d5400
ushisha <usisha.s@gmail.com>
2021-07-01 12:16:25 +0100

    add:default image for event

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1a221bd4fefd8911278"; }
+/******/ 		__webpack_require__.h = function() { return "d3f8696933f183f81b58"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
